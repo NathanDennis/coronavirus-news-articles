48d06a5181fd53feb7cea7306fc6b45a8f80b97e
NathanDennis <nathdennis@googlemail.com>
2020-03-22 06:05:01 -0400

    added dynamic country source switching to render different articles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-    <div class="ui grid article-wrapper">
-        <NewsArticleCard v-for="article in articles" :key="article.id"
+    <div class="buttons">
+      <button class="ui inverted primary button" v-on:click="articlesToRender='UK', currentlyRendered='Articles from Great Britain'">UK Articles</button>
+      <button class="ui inverted button" v-on:click="articlesToRender='USA', currentlyRendered='Articles from The United States'">USA Articles</button>
+      <h1 class="currently-rendered">{{ currentlyRendered }}</h1>
+    </div>
+
+
+    <!-- USA ARTICLES -->
+    <div class="ui grid article-wrapper" v-if="articlesToRender==='USA'">
+          <NewsArticleCard v-for="article in UnitedStatesArticles" :key="article.id"
+          :title="article.title" 
+          :description="article.description"
+          :urlToImage="article.urlToImage"
+          :url="article.url"
+        />
+    </div>
+
+    <!-- UK ARTICLES -->
+    <div class="ui grid article-wrapper" v-else>
+          <NewsArticleCard v-for="article in UKArticles" :key="article.id"
           :title="article.title" 
           :description="article.description"
           :urlToImage="article.urlToImage"
@@ -16,8 +34,7 @@
 import axios from 'axios'
 
 // Component imports
-import NewsArticleCard from './components/NewsArticleCard'
-
+import NewsArticleCard from './components/NewsArticleCard' 
 export default {
   name: 'App',
   components: {
@@ -25,13 +42,18 @@ export default {
   },
   data() {
     return {
-      articles: []
+      UnitedStatesArticles: [],
+      UKArticles: [],
+      articlesToRender: 'USA',
+      currentlyRendered: 'Articles from the United States'
     }
   },
   mounted() {
-    axios.get('https://nd-coronavirus-article-data.herokuapp.com/')
+    // axios.get('https://nd-coronavirus-article-data.herokuapp.com/')
+    axios.get('http://localhost:3000')
       .then((response) => {
-        this.articles = response.data
+        this.UnitedStatesArticles = response.data.UnitedStatesArticleData
+        this.UKArticles = response.data.UKArticleData
         console.log('build success')
     })
   }
@@ -47,6 +69,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.currently-rendered {
+  color: white;
+  text-decoration: underline;
+}
+
+.buttons {
+  margin-bottom: 2rem;
 }
 
 li {
