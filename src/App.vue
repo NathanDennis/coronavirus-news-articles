6b66693c1a891a6b28861ed1c9536ff6f419d89e
NathanDennis <nathdennis@googlemail.com>
2020-03-22 04:23:08 -0400

    committing static folder

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <div class="ui grid article-wrapper">
+        <NewsArticleCard v-for="article in articles" :key="article.id"
+          :title="article.title" 
+          :description="article.description"
+          :urlToImage="article.urlToImage"
+          :url="article.url"
+        />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+// Component imports
+import NewsArticleCard from './components/NewsArticleCard'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewsArticleCard
+  },
+  data() {
+    return {
+      articles: []
+    }
+  },
+  mounted() {
+    axios.get('https://nd-coronavirus-article-data.herokuapp.com/')
+      .then((response) => {
+        this.articles = response.data
+        console.log('build success')
+    })
   }
 }
+
 </script>
 
 <style>
@@ -24,5 +47,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+li {
+  list-style: none;
 }
 </style>
---
+++ src/components/NewsArticleCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="ui card article content">
+        <a class="image card-image" :href="url" target="_blank" rel="noopener noreferrer">
+            <img :src="urlToImage" alt="news article thumbnail">
+        </a>
+        
+        <p >
+            <a :href="url" target="blank" class="header">{{title}}</a>
+        </p>
+        <div class="meta">
+            <span class="date">{{publishedAt}}</span>
+        </div>
+        <div class="description">
+            {{description}}
+        </div>
+    </div>
+</template>
+
+<!-- sourceName = article.source.name  -->
+
+<script>
+export default {
+    name: 'NewsArticleCard',
+    props: ['title', 'description', 'url', 'urlToImage', 'publishedAt', 'sourceName']
+}
+</script>
+
+<style>
+.card-image {
+    margin-bottom: 1rem;
+}
+
+.article .description, .header{
+    font-size: 1.25rem;
+}
+</style>
