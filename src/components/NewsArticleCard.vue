44116bbd466d4f0fa9b88c848bb9ab16a4a72594
Simon Skinner <simon@vult.uk>
2020-10-27 11:26:34 +0000

    Image Containment and content padding

--- src/components/NewsArticleCard.vue
+++ src/components/NewsArticleCard.vue
@@ -1,29 +1,55 @@
 <template>
-    <div class="ui card article content">
-        <a class="image card-image" :href="url" target="_blank" rel="noopener noreferrer">
-            <img :src="urlToImage" alt="news article thumbnail">
-        </a>
-        
-        <p><a :href="url" target="blank" class="header">{{title}}</a></p>
-        <div class="description">
-            {{description}}
-        </div>
+  <div class="ui card article content">
+    <a
+      class="image card-image"
+      :href="url"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img :src="urlToImage" alt="news article thumbnail" />
+    </a>
+
+    <div class="content-text">
+      <p>
+        <a :href="url" target="blank" class="header">{{ title }}</a>
+      </p>
+      <div class="description">
+        {{ description }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'NewsArticleCard',
-    props: ['title', 'description', 'url', 'urlToImage', 'publishedAt', 'sourceName']
-}
+  name: 'NewsArticleCard',
+  props: [
+    'title',
+    'description',
+    'url',
+    'urlToImage',
+    'publishedAt',
+    'sourceName',
+  ],
+};
 </script>
 
 <style>
 .card-image {
-    margin-bottom: 1rem;
+  margin-bottom: 1rem;
 }
 
-.article .description, .header{
-    font-size: 1.25rem;
+.card-image img {
+  object-fit: cover;
+  height: 12rem !important;
+}
+
+.article .description,
+.header {
+  font-size: 1.25rem;
+}
+
+.content-text {
+  margin: 0 0.5rem 1rem 0.5rem;
 }
 </style>
